0045467d3c074da85c177b3b33fe9f7b54cc4bf6
Lê Hải Âu <lehaiau15042003@gmail.com>
2026-05-01 16:02:52 +0700

    fix: delete .env

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_APP_API_URL;
+const API_URL = 'http://localhost:8080/api/v1';
 
 const postService = {
     getAllPosts: async () => {
